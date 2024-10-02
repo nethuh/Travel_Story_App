0de3bea2415ec82aef8f99c13d64813ec2c27413
Sayani nethmi <nethmihewage2002@gmail.com>
2024-10-02 18:14:07 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt");
+const express = require("express");
+const cors = require("cors");
+
+const jwt = require("jsonwebtoken");
+
+mongoose.connect(config.connectionString);
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+//create Account
+app.get("/create-account", async (req, res) => {
+});
+
+app.listen(8000);
+module.exports = app;
